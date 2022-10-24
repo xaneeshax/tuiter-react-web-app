bd9f29718e6ec290c9ccda5a37f3600afc0f7d9d
xaneeshax <aneesha.sreerama@gmail.com>
2022-10-23 21:26:58 -0400

    Change Links a6

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -7,37 +7,37 @@ const NavigationSidebar = (
 ) => {
  return (
    <div className="list-group">
-     <a className="list-group-item" href="/">Tuiter</a>
+     <a className="list-group-item" href="https://github.com/xaneeshax/tuiter-react-web-app/tree/a6">Tuiter</a>
      <a className={`list-group-item
-                    ${active === 'home'?'active':''}`} href="/">
+                    ${active === 'home'?'active':''}`} href="https://github.com/xaneeshax/tuiter-react-web-app/tree/a6">
        Home
      </a>
      <a className={`list-group-item
-                    ${active === 'explore'?'active':''}`} href="/">
+                    ${active === 'explore'?'active':''}`} href="https://github.com/xaneeshax/tuiter-react-web-app/tree/a6">
        Explore
      </a>
      <a className={`list-group-item
-                    ${active === 'notifications'?'active':''}`} href="/">
+                    ${active === 'notifications'?'active':''}`} href="https://github.com/xaneeshax/tuiter-react-web-app/tree/a6">
        Notifications
      </a>
      <a className={`list-group-item
-                    ${active === 'messages'?'active':''}`} href="/">
+                    ${active === 'messages'?'active':''}`} href="https://github.com/xaneeshax/tuiter-react-web-app/tree/a6">
        Messages
      </a>
      <a className={`list-group-item
-                    ${active === 'bookmarks'?'active':''}`} href="/">
+                    ${active === 'bookmarks'?'active':''}`} href="https://github.com/xaneeshax/tuiter-react-web-app/tree/a6">
        Bookmarks
      </a>
      <a className={`list-group-item
-                    ${active === 'lists'?'active':''}`} href="/">
+                    ${active === 'lists'?'active':''}`} href="https://github.com/xaneeshax/tuiter-react-web-app/tree/a6">
        Lists
      </a>
      <a className={`list-group-item
-                    ${active === 'profile'?'active':''}`} href="/">
+                    ${active === 'profile'?'active':''}`} href="https://github.com/xaneeshax/tuiter-react-web-app/tree/a6">
        Profile
      </a>
      <a className={`list-group-item
-                    ${active === 'more'?'active':''}`} href="/">
+                    ${active === 'more'?'active':''}`} href="https://github.com/xaneeshax/tuiter-react-web-app/tree/a6">
        More
      </a>
    </div>
