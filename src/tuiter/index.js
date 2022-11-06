1b523d41d7fa9edc3438db458d8db0dc9214f34b
xaneeshax <aneesha.sreerama@gmail.com>
2022-11-06 17:07:20 -0500

    a7 changes

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ExploreComponent from "./explore";
 import HomeComponent from "./home"
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
