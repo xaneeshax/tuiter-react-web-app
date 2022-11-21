df6de94aba2905ddb0d5a673762aa7993e4daee3
xaneeshax <aneesha.sreerama@gmail.com>
2022-11-20 23:12:59 -0500

    a8 fix loading

--- src/tuiter/tuits/TuitsList.js
+++ src/tuiter/tuits/TuitsList.js
@@ -5,7 +5,7 @@ import {findTuitsThunk}
   from "../../services/tuits-thunks";
 
 const TuitsList = () => {
-  // const {tuits, loading} = useSelector(state => state.tuitsData)
+  const {_, loading} = useSelector(state => state.tuitsData)
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(findTuitsThunk())
